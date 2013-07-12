2a7ad92bf6bff09aeec76107b303d16352dd80a6
Jose Sanchez <jose.b.sanchez.e@gmail.com>
2013-07-12 16:00:06 -0400

    Inicio de Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pruebajose::Application.config.secret_token = 'c45da86359f1d4f20f01fb71d8b808dfdf6bef931e25a8939b1761cf9ab694f3fc1422f5c60a0b66a092cb9291b7c6973996ff6039fc1a5da055295945165c09'
